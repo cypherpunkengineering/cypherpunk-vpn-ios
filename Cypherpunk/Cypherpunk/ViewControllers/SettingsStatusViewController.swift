1d9885167629fd33bbfe2dceac6187ca74592368
Keisuke Kimura <kimura@uphyca.com>
2016-08-08 20:57:18 +0900

    Bind Status Labels & add Brank Help Me Choose Section

--- Cypherpunk/Cypherpunk/ViewControllers/SettingsStatusViewController.swift
+++ Cypherpunk/Cypherpunk/ViewControllers/SettingsStatusViewController.swift
@@ -12,6 +12,14 @@ class SettingsStatusViewController: UITableViewController, PageContent {
 
     var pageIndex: Int = 1
     
+    @IBOutlet weak var connectionTimeLabelButton: UIButton!
+    
+    @IBOutlet weak var originalIPAddressLabel: UILabel!
+    @IBOutlet weak var originalLocaleLabelButton: UIButton!
+
+    @IBOutlet weak var newIPAddressLabel: UILabel!
+    @IBOutlet weak var newLocalLabelButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,6 +33,22 @@ class SettingsStatusViewController: UITableViewController, PageContent {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func changeIPAddressAction(sender: AnyObject) {
+    }
+    
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 17))
+        let titleLabel = UILabel(frame: CGRect(x: 20, y: 9, width: 300, height: 17))
+        
+        titleLabel.font = R.font.dosisMedium(size: 14)
+        titleLabel.text = self.tableView(tableView, titleForHeaderInSection: section)
+        titleLabel.textColor = UIColor(red: 204.0 / 255.0 , green: 204.0 / 255.0 , blue: 204.0 / 255.0 , alpha: 1.0)
+        
+        view.addSubview(titleLabel)
+        
+        return view
     }
 
 }
