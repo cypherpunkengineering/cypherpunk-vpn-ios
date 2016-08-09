a291f629f3771ef73b197ce7e810d80c3cb02a1c
Keisuke Kimura <kimura@uphyca.com>
2016-08-09 21:07:20 +0900

    Create Connection Button

--- Cypherpunk/Cypherpunk/AppDelegate.swift
+++ Cypherpunk/Cypherpunk/AppDelegate.swift
@@ -24,7 +24,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        
+        VPNConfigurationCoordinator.start {
+            
+        }
         SVProgressHUD.setDefaultStyle(SVProgressHUDStyle.Light)
         SVProgressHUD.setDefaultMaskType(SVProgressHUDMaskType.Gradient)
 
