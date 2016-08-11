f45b522fc0bf5c821e5d66097ef543a02820e4b4
Keisuke Kimura <kimura@uphyca.com>
2016-08-11 19:45:06 +0900

    Add 0 or 1 background layer(no animation)

---
+++ Cypherpunk/Cypherpunk/ViewControllers/AnimationViewController.swift
@@ -0,0 +1,51 @@
+//
+//  AnimationViewController.swift
+//  Cypherpunk
+//
+//  Created by KeisukeKimura on 2016/08/11.
+//  Copyright © 2016年 Cypherpunk. All rights reserved.
+//
+
+import UIKit
+import CoreGraphics
+
+class AnimationViewController: UIViewController {
+
+    let size = CGSizeMake(15,21)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        let horizontalItemsCount = Int(floor(view.frame.width / size.width))
+        let verticalItemsCount = Int(floor(view.frame.height / size.height))
+        let font = R.font.dosisRegular(size: 20)
+
+        for x in 0...horizontalItemsCount {
+            for y in 0...verticalItemsCount {
+                
+                let number: Int = random() % 2
+                let layoutPoint = CGPoint(x: CGFloat(x) * size.width , y: CGFloat(y) * size.height)
+                let frame = CGRect(origin: layoutPoint, size: size)
+                
+                let textLayer = CATextLayer()
+                textLayer.string = String(number)
+                textLayer.font = font
+                textLayer.fontSize = 20
+                textLayer.foregroundColor = UIColor.whiteThemeNavigationColor().colorWithAlphaComponent(0.15).CGColor
+                textLayer.frame = frame
+                
+                textLayer.alignmentMode = kCAAlignmentCenter
+                textLayer.contentsScale = UIScreen.mainScreen().scale
+                self.view.layer.addSublayer(textLayer)
+            }
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
