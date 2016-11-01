2fc792878236df0c38426187076a1e9a8937ee75
Keisuke Kimura <kimura@uphyca.com>
2016-11-01 19:00:41 +0900

    Fix Gradation

--- Cypherpunk/Cypherpunk/View/ThemedBackgroundView.swift
+++ Cypherpunk/Cypherpunk/View/ThemedBackgroundView.swift
@@ -16,12 +16,15 @@ class ThemedBackgroundView: UIView {
             DispatchQueue.main.async(execute: {
                 let topColor = UIColor(red: 118.0 / 255.0, green: 168.0 / 255.0 , blue: 231.0 / 255.0 , alpha: 1.0)
                 let bottomColor = UIColor(red: 3.0 / 255.0, green: 70.0 / 255.0 , blue: 152.0 / 255.0 , alpha: 1.0)
+
                 let gradientColors: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
                 
                 let gradientLayer: CAGradientLayer = CAGradientLayer()
                 gradientLayer.colors = gradientColors
                 gradientLayer.frame = self.layer.frame
-                
+                gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+                gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+
                 if self.gradientLayer != nil {
                     self.gradientLayer.removeFromSuperlayer()
                     self.gradientLayer = nil
@@ -33,28 +36,6 @@ class ThemedBackgroundView: UIView {
             })
         }
     }
-//    override var bounds: CGRect {
-//        didSet {
-//            DispatchQueue.main.async(execute: {
-//                let topColor = UIColor(red: 118.0 / 255.0, green: 168.0 / 255.0 , blue: 231.0 / 255.0 , alpha: 1.0)
-//                let bottomColor = UIColor(red: 3.0 / 255.0, green: 70.0 / 255.0 , blue: 152.0 / 255.0 , alpha: 1.0)
-//                let gradientColors: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
-//                
-//                let gradientLayer: CAGradientLayer = CAGradientLayer()
-//                gradientLayer.colors = gradientColors
-//                gradientLayer.frame = self.layer.frame
-//                
-//                if self.gradientLayer != nil {
-//                    self.gradientLayer.removeFromSuperlayer()
-//                    self.gradientLayer = nil
-//                }
-//                
-//                self.gradientLayer = gradientLayer
-//                
-//                self.layer.insertSublayer(gradientLayer, at: 0)
-//            })
-//        }
-//    }
 
     override func awakeFromNib() {
         configureView()
@@ -62,13 +43,18 @@ class ThemedBackgroundView: UIView {
     
     func configureView() {
         DispatchQueue.main.async(execute: {
+//            let topColor = UIColor(red: 118.0 / 255.0, green: 168.0 / 255.0 , blue: 231.0 / 255.0 , alpha: 1.0)
+//            let bottomColor = UIColor(red: 3.0 / 255.0, green: 70.0 / 255.0 , blue: 152.0 / 255.0 , alpha: 1.0)
             let topColor = UIColor(red: 118.0 / 255.0, green: 168.0 / 255.0 , blue: 231.0 / 255.0 , alpha: 1.0)
             let bottomColor = UIColor(red: 3.0 / 255.0, green: 70.0 / 255.0 , blue: 152.0 / 255.0 , alpha: 1.0)
+
             let gradientColors: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
             
             let gradientLayer: CAGradientLayer = CAGradientLayer()
             gradientLayer.colors = gradientColors
             gradientLayer.frame = self.layer.frame
+            gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+            gradientLayer.endPoint = CGPoint(x: 1, y: 1)
 
             if self.gradientLayer != nil {
                 self.gradientLayer.removeFromSuperlayer()
