1ddb28b2cf4f3aea665023db746a7e0b35d86218
Julie Ann Sakuda <julie@slickage.com>
2017-02-07 01:31:21 -1000

    Updating Realm to version 2.4.2.

--- Cypherpunk/Cypherpunk/Util/ServerPingHelper.swift
+++ Cypherpunk/Cypherpunk/Util/ServerPingHelper.swift
@@ -18,10 +18,13 @@ class ServerPingHelper : NSObject, GBPingDelegate {
         
         for server in servers {
             print(server.ovHostname)
+            
+            let hostname = server.ovHostname
+            
             let delegate = PingHandler(serverId: server.id)
             
             let ping = GBPing()
-            ping.host = server.ovHostname
+            ping.host = hostname
             ping.timeout = 1.0
             ping.delegate = delegate
             
@@ -29,6 +32,9 @@ class ServerPingHelper : NSObject, GBPingDelegate {
                 if (success) {
                     ping.startPinging()
                     pingHandlers[ping] = delegate
+                }
+                else {
+                    print("Failed to setup pinger for \(hostname) - \(error)")
                 }
             })
         }
@@ -54,32 +60,49 @@ class PingHandler : NSObject, GBPingDelegate {
             pinger.stop()
             ServerPingHelper.pingHandlers.removeValue(forKey: pinger)
             
-            // evaluate all the rtts
-            let minRtt = rtts.min()
-            
-            let realm = try! Realm()
-            let region = realm.object(ofType: Region.self, forPrimaryKey: serverId)
-            
-            do {
-                try realm.write {
-                    region?.latencySeconds = minRtt!
-                }
-            } catch {
-                print("Failed to udpate latency for \(serverId): \(error)")
-            }
+            writeLatencyToRealm()
         }
     }
     
     // TODO: How should errors and timeouts be handled? Should the last known latency be removed from Realm?
-//    func ping(_ pinger: GBPing!, didTimeoutWith summary: GBPingSummary!) {
-//        
-//    }
-//    
-//    func ping(_ pinger: GBPing!, didReceiveUnexpectedReplyWith summary: GBPingSummary!) {
-//        
-//    }
-//    
-//    func ping(_ pinger: GBPing!, didFailWithError error: Error!) {
-//        
-//    }
+    func ping(_ pinger: GBPing!, didTimeoutWith summary: GBPingSummary!) {
+        print("Timeout > \(serverId)")
+        
+        // If a timeout occurs count it as a 10 second ping time
+        rtts.append(10)
+        
+        // stop the pinging after the 3rd ping and save the lowest latency
+        if (summary.sequenceNumber == 2) {
+            pinger.stop()
+            ServerPingHelper.pingHandlers.removeValue(forKey: pinger)
+            
+            writeLatencyToRealm()
+        }
+    }
+    
+    func ping(_ pinger: GBPing!, didReceiveUnexpectedReplyWith summary: GBPingSummary!) {
+        print("Unexpected Reply > \(serverId)")
+        pinger.stop() // not sure what happened, stop pinging the server
+    }
+    
+    func ping(_ pinger: GBPing!, didFailWithError error: Error!) {
+        print("Error > \(serverId) - \(error)")
+        pinger.stop() // error occured, stop pinging the server
+    }
+    
+    private func writeLatencyToRealm() {
+        // evaluate all the rtts
+        let minRtt = rtts.min()
+        
+        let realm = try! Realm()
+        let region = realm.object(ofType: Region.self, forPrimaryKey: serverId)
+        
+        do {
+            try realm.write {
+                region?.latencySeconds = minRtt!
+            }
+        } catch {
+            print("Failed to udpate latency for \(serverId): \(error)")
+        }
+    }
 }
