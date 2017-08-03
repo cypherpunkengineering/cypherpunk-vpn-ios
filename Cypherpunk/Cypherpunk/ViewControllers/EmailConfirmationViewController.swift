a1eca953336c8ffc154275d6e04dcd3a14a3fb7a
Julie Ann Sakuda <julie@slickage.com>
2017-08-03 02:34:26 -1000

    Updating email confirmation screen.
    Fixing error with password being nil.

--- Cypherpunk/Cypherpunk/ViewControllers/EmailConfirmationViewController.swift
+++ Cypherpunk/Cypherpunk/ViewControllers/EmailConfirmationViewController.swift
@@ -12,21 +12,42 @@ import APIKit
 class EmailConfirmationViewController: UIViewController {
 
     @IBOutlet weak var resendEmailButton: UIButton!
+    @IBOutlet weak var checkAgainButton: UIButton!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailPanelView: UIView!
+    @IBOutlet weak var loadingAnimationView: LoadingAnimationView!
+    
+    @IBOutlet weak var checkEmailLabel: UILabel!
+    @IBOutlet weak var confirmEmailLabel: UILabel!
     
     var mailAddress: String!
+    
     var password: String!
+    
+    private var repeatCount: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-//        let attributes: [String: AnyObject] = [
-//            NSUnderlineStyleAttributeName: NSUnderlineStyle.styleSingle.rawValue as AnyObject,
-//            NSForegroundColorAttributeName: UIColor.white
-//        ]
-//        let resendEmailButtonAttribute = NSAttributedString(string: "Resend Email", attributes: attributes)
-//        resendEmailButton?.setAttributedTitle(resendEmailButtonAttribute, for: .normal)
-
+        self.emailLabel.text = mailAddress
+        
+        let refreshString = String.fontAwesomeIcon(name: .refresh)
+        let emailString = String.fontAwesomeIcon(name: .envelope)
+        
+        let fontAwesomeFont = UIFont.fontAwesome(ofSize: 16)
+        let textFont = R.font.dosisRegular(size: 18)!
+        let textColor = UIColor(red: 111 / 255.0, green: 133 / 255.0, blue: 137 / 255.0, alpha: 1.0)
+        
+        let checkAgainAttributedString = NSMutableAttributedString(string: refreshString + "   Check again", attributes: [NSFontAttributeName: textFont, NSForegroundColorAttributeName: textColor])
+        
+        checkAgainAttributedString.addAttributes([NSFontAttributeName: fontAwesomeFont], range: NSRange(location: 0, length: 1))
+        self.checkAgainButton.setAttributedTitle(checkAgainAttributedString, for: .normal)
+        
+        
+        let resendAttributedString = NSMutableAttributedString(string: emailString + "   Resend email", attributes: [NSFontAttributeName: textFont, NSForegroundColorAttributeName: textColor])
+        
+        resendAttributedString.addAttributes([NSFontAttributeName: fontAwesomeFont], range: NSRange(location: 0, length: 1))
+        self.resendEmailButton.setAttributedTitle(resendAttributedString, for: .normal)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -62,12 +83,21 @@ class EmailConfirmationViewController: UIViewController {
         let _ = self.navigationController?.popToRootViewController(animated: true)
     }
     
+    @IBAction func checkAgainAction(_ sender: Any) {
+        startTimer()
+    }
+    
     @IBAction func resendAction(_ sender: Any) {
         let confirmation = ConfirmEmailRequest(email: mailAddress)
         Session.send(confirmation)
     }
 
     func update() {
+        if repeatCount > 4 {
+            cancelTimer()
+            return
+        }
+        
         guard let session = mainStore.state.accountState.session else {
             return
         }
@@ -75,12 +105,12 @@ class EmailConfirmationViewController: UIViewController {
         Session.send(request) { result in
             if case let .success(response) = result {
                 if response.account.confirmed {
+                    mainStore.dispatch(AccountAction.login(response: response))
                     let region = RegionListRequest(session: session, accountType: response.account.type)
                     Session.send(region) { result in
                         if case .success = result {
                             var response = response
                             response.session = session
-                            mainStore.dispatch(AccountAction.login(response: response))
                             
                             let destination = R.storyboard.walkthrough.instantiateInitialViewController()
                             self.navigationController?.pushViewController(destination!, animated: true)
@@ -89,12 +119,23 @@ class EmailConfirmationViewController: UIViewController {
                 }
             }
         }
+        repeatCount += 1
     }
     var timer: Timer! = nil
     func startTimer() {
         if timer == nil {
             self.timer = Timer.scheduledTimer(timeInterval: 10.0, target: self, selector: #selector(EmailConfirmationViewController.update), userInfo: nil, repeats: true)
             RunLoop.current.add(timer, forMode: .commonModes)
+        }
+        
+        UIView.animate(withDuration: 0.3) { 
+            self.checkEmailLabel.layer.opacity = 1.0
+            self.loadingAnimationView.layer.opacity = 1.0
+            
+            self.confirmEmailLabel.layer.opacity = 0.0
+            self.resendEmailButton.layer.opacity = 0.0
+            self.checkAgainButton.layer.opacity = 0.0
+            self.resendEmailButton.layer.opacity = 0.0
         }
     }
     
@@ -103,6 +144,18 @@ class EmailConfirmationViewController: UIViewController {
             timer.invalidate()
             timer = nil
         }
+        
+        UIView.animate(withDuration: 0.3) { 
+            self.checkEmailLabel.layer.opacity = 0.0
+            self.loadingAnimationView.layer.opacity = 0.0
+            
+            self.confirmEmailLabel.layer.opacity = 1.0
+            self.resendEmailButton.layer.opacity = 1.0
+            self.checkAgainButton.layer.opacity = 1.0
+            self.resendEmailButton.layer.opacity = 1.0
+        }
+        
+        repeatCount = 0
     }
     
 
