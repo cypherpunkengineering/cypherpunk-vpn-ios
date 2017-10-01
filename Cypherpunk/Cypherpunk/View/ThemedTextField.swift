6b6b8c415b1b5f68f7a25ca7ebf1582cb75187c7
Julie Ann Sakuda <julie@slickage.com>
2017-09-30 15:36:41 -1000

    Custom clear button for login text field.

--- Cypherpunk/Cypherpunk/View/ThemedTextField.swift
+++ Cypherpunk/Cypherpunk/View/ThemedTextField.swift
@@ -19,6 +19,20 @@ class ThemedTextField: UITextField {
     func configureView() {
         self.attributedPlaceholder = NSAttributedString(string:self.placeholder ?? "",
                                                                         attributes:[NSForegroundColorAttributeName: UIColor.white.withAlphaComponent(0.5)])
-    }
+        
+        let clearButton = UIButton(type: .custom)
+        clearButton.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        clearButton.titleLabel?.font = UIFont.fontAwesome(ofSize: 14)
+        clearButton.setTitle(String.fontAwesomeIcon(code: "fa-times-circle"), for: .normal)
+        clearButton.setTitleColor(UIColor.greenyBlue, for: .normal)
+        clearButton.addTarget(self, action: #selector(clearTextField), for: .touchUpInside)
 
+        self.rightView = clearButton
+        
+        self.rightViewMode = .whileEditing
+    }
+    
+    @IBAction func clearTextField() {
+        self.text = ""
+    }
 }
